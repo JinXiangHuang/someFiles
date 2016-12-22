8d7e8bc266d34b652afb8289d7f5085a7ea371a0
huangjinxiang <445106527@qq.com>
2016-12-22 17:06:56 +0800

    set

---
+++ iOSFoundation/iOSFoundation/BasicFramework/MVC/ViewControllers/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSFoundation
+//
+//  Created by mr-tech on 16/12/22.
+//  Copyright © 2016年 mr-tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSFoundation/iOSFoundation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSFoundation
+//
+//  Created by mr-tech on 16/12/22.
+//  Copyright © 2016年 mr-tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
